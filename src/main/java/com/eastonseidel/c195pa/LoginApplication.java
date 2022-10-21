285cbf4b149330a11b8273462ef8e7b88dbc34ee
Easton Seidel <eseidel1357@gmail.com>
2022-10-21 12:44:49 -0700

    Started the code for login controller and app

--- src/main/java/com/eastonseidel/c195pa/LoginApplication.java
+++ src/main/java/com/eastonseidel/c195pa/LoginApplication.java
@@ -1,16 +1,25 @@
 package com.eastonseidel.c195pa;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
+
 public class LoginApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }
--- src/main/java/com/eastonseidel/c195pa/LoginController.java
+++ src/main/java/com/eastonseidel/c195pa/LoginController.java
@@ -1,16 +1,7 @@
 package com.eastonseidel.c195pa;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-public class LoginController extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+public class LoginController {
 }
