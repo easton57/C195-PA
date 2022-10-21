e6227513adc2e96f9a4a10429e31017ef86dc1b2
Easton Seidel <eseidel1357@gmail.com>
2022-10-21 09:53:38 -0700

    Created Login Files

---
+++ src/main/java/com/eastonseidel/c195pa/LoginController.java
@@ -0,0 +1,16 @@
+package com.eastonseidel.c195pa;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
