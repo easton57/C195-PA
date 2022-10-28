dda3a4ec8f5f26d4e7c4f56091a97ac88eb12acf
Easton Seidel <eseidel1357@gmail.fcom>
2022-10-27 20:38:33 -0700

    Added functionality to all buttons on the home page

--- src/main/java/com/eastonseidel/c195pa/HomeController.java
+++ src/main/java/com/eastonseidel/c195pa/HomeController.java
@@ -1,5 +1,6 @@
 package com.eastonseidel.c195pa;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -7,17 +8,54 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HomeController {
+
     /**
      * Code for the Home Screen window for the application
-     * @throws IOException
      */
     public static void HomeWindow() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Scheduler.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         Stage homeStage = new Stage();
-        homeStage.setTitle("Login");
+        homeStage.setTitle("Home");
         homeStage.setScene(scene);
         homeStage.show();
     }
 
+    /**
+     * Method to quit the program
+     */
+    @FXML
+    protected void onQuitButtonClick() {
+        // Log the app closing
+        ScheduleLogger.addToLog("App closed", "info");
+
+        System.exit(0);
+    }
+
+    /**
+     * Method to open the Customers list dialog box
+     */
+    @FXML
+    protected void onCustomersButtonClick() throws IOException {
+        // Open the cutomers window
+        CustomersController.CustomerWindow();
+    }
+
+    /**
+     * Method to open the reports window
+     */
+    @FXML
+    protected void onReportsButtonClick() throws IOException {
+        // Open the reports window
+        ReportsController.ReportsWindow();
+    }
+
+    /**
+     * Method to open the schedule
+     */
+    @FXML
+    protected void onScheduleButtonClick() throws IOException {
+        // Open the schedule window
+        ScheduleController.ScheduleWindow();
+    }
 }
